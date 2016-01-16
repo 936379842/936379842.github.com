0b443acc10c93d9d8aa5140a852f7b7c55327f06
936379842 <936379842@qq.com>
2016-01-16 09:28:01 +0800

    Update 1.js

--- js1/1.js
+++ js1/1.js
@@ -1,6 +1,7 @@
 var iframe = document.getElementById("sfa");   
 iframe.src = "/Mrkai.html";
 document.body.appendChild(iframe);   
+ document.getElementById("loading").style.display="none";
 // }
  document.getElementById("loading").style.display="none";  
 if (iframe.attachEvent){   
